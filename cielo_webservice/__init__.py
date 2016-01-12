43bb3f66a52df11e22e265afbaeddf496cc7f6df
Allisson Azevedo <allisson@gmail.com>
2016-01-12 14:45:25 -0300

    Versão 1.0.1.

--- cielo_webservice/__init__.py
+++ cielo_webservice/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '1.0.1.dev0'
+__version__ = '1.0.1'
 __description__ = 'Biblioteca para trabalhar com a versão Webservice 1.5 da Cielo.'
 __author__ = 'Allisson Azevedo'
 __author_email__ = 'allisson@gmail.com'
